b2f34f5ddd18d356ea29dfbefc549150e17c6252
iengai <iamibe.kai@gmail.com>
2017-03-09 00:14:34 +0900

    add link to auth from welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,40 +5,68 @@
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
+                <!-- Styles -->
         <style>
             html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
                 font-weight: 100;
-                font-family: 'Lato';
+                height: 100vh;
+                margin: 0;
             }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
+            .full-height {
+                height: 100vh;
             }
-
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+            .position-ref {
+                position: relative;
+            }
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
             .content {
                 text-align: center;
-                display: inline-block;
             }
-
             .title {
-                font-size: 96px;
+                font-size: 84px;
+            }
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+            .m-b-md {
+                margin-bottom: 30px;
             }
         </style>
     </head>
     <body>
-        <div class="container">
+        <div class="flex-center position-ref full-height">
+
+
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title m-b-md">
+                    RPG online
+                </div>
+
+                <div class="links">
+
+                        <a href="{{ url('/auth/login') }}">Login</a>
+                        <a href="{{ url('/auth/register') }}">Register</a>
+
+                </div>
+
             </div>
         </div>
     </body>
